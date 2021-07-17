39e3e57dcfcfb9e18ca585122e6f222256677901
Sanjay <83747055+sanjaykumar584@users.noreply.github.com>
2021-07-17 12:32:04 +0530

    Add initial files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var theme = 0;
+
+$("#calculator").css("max-width", $("#calculator").width());
+
+$(".btn").on("click", function() {
+    if ($(this).hasClass("white-key")) {
+        if (!(document.querySelector("#calculation").innerHTML.length >= 12)) {
+            if (document.querySelector("#calculation").innerHTML == "0") {
+                document.querySelector("#calculation").innerHTML = "";
+            }
+            document.querySelector("#calculation").innerHTML += this.innerHTML;
+        }
+    } else if ($(this).hasClass("del")) {
+        $("#calculation").html((document.querySelector("#calculation").innerHTML).slice(0, -1));
+        if (document.querySelector("#calculation").innerHTML.length == 0) {
+            $("#calculation").html("0");
+        }
+    } else if ($(this).hasClass("red-key")) {
+        if (document.querySelector("#calculation").innerHTML.length != 0) {
+            $("#calculation").html("STOP Slouching!");
+        }
+    } else if ($(this).hasClass("reset")) {
+        $("#calculation").html("0");
+    }
+});
+
+$("#toggle").on("click", function() {
+    if (theme == 0) {
+        theme += 1;
+    } else if (theme == 1) {
+        theme += 1;
+    } else {
+        theme = 0;
+    }
+    switch (theme) {
+        case 0:
+            $("#toggle").css("text-align", "left");
+            break;
+        case 1:
+            $("#toggle").css("text-align", "center");
+            break;
+        case 2:
+            $("#toggle").css("text-align", "right");
+            break;
+    
+        default:
+            break;
+    }
+});
+
